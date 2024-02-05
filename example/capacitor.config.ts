5f11345df915440625ccbb815e680800537b5b57
ebarooni <barooni.eh@gmail.com>
2024-02-05 23:13:31 +0100

    added ionic and capacitor to the example app

---
+++ example/capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'dev.barooni.CapacitorCalendarExample',
+  appName: 'example',
+  webDir: 'dist/example/browser',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
