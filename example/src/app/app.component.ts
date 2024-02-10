ad06e10caea388997885aece4132613c0bc4810d
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-02-11 00:05:09 +0100

    created methods list component

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -1,37 +1,23 @@
 import { Component } from '@angular/core';
-import {
-  IonApp,
-  IonButton,
-  IonContent,
-  IonHeader, IonIcon,
-  IonItem, IonLabel,
-  IonList, IonListHeader,
-  IonTitle,
-  IonToolbar
-} from "@ionic/angular/standalone";
-import {CalendarEventActionResult, CapacitorCalendar} from "@ebarooni/capacitor-calendar";
-import {addIcons} from "ionicons";
-import { informationCircle } from 'ionicons/icons';
+import {IonApp, IonContent} from "@ionic/angular/standalone";
 import {ThemeToggleComponent} from "./theme-toggle/theme-toggle.component";
 import {HeaderComponent} from "./header/header.component";
 import {CondensedHeaderComponent} from "./condensed-header/condensed-header.component";
+import {MethodsListComponent} from "./methods-list/methods-list.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonApp, IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonList, IonItem, IonLabel, IonListHeader, IonIcon, ThemeToggleComponent, HeaderComponent, CondensedHeaderComponent],
+  imports: [
+    IonApp,
+    IonContent,
+    ThemeToggleComponent,
+    HeaderComponent,
+    CondensedHeaderComponent,
+    MethodsListComponent
+  ],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
   readonly headerTitle = 'CapacitorCalendar';
-
-  constructor() {
-    addIcons({
-      'information-circle': informationCircle
-    });
-  }
-
-  public createEventWithPrompt(): Promise<{ result: CalendarEventActionResult }> {
-    return CapacitorCalendar.createEventWithPrompt();
-  }
 }
