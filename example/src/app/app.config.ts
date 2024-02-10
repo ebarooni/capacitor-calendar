d82941efe92e5433a40fbc05574c1b4562f9b1df
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-02-10 23:53:41 +0100

    integrated new header components

--- example/src/app/app.config.ts
+++ example/src/app/app.config.ts
@@ -3,5 +3,9 @@ import { ApplicationConfig } from '@angular/core';
 import { provideIonicAngular } from '@ionic/angular/standalone';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideIonicAngular({})]
+  providers: [
+    provideIonicAngular({
+      mode: 'ios'
+    })
+  ]
 };
