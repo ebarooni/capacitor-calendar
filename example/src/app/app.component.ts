6fd409203eb0a1013a986c0f01652076c269b0ed
ebarooni <barooni.eh@gmail.com>
2024-02-10 23:53:41 +0100

    integrated new header components

--- example/src/app/app.component.ts
+++ example/src/app/app.component.ts
@@ -6,39 +6,29 @@ import {
   IonHeader, IonIcon,
   IonItem, IonLabel,
   IonList, IonListHeader,
-  IonTitle, IonToggle,
+  IonTitle,
   IonToolbar
 } from "@ionic/angular/standalone";
 import {CalendarEventActionResult, CapacitorCalendar} from "@ebarooni/capacitor-calendar";
 import {addIcons} from "ionicons";
-import { informationCircle, moon } from 'ionicons/icons';
-import {BehaviorSubject, fromEvent, map, merge, Observable, tap} from "rxjs";
-import { LetDirective } from '@ngrx/component';
+import { informationCircle } from 'ionicons/icons';
+import {ThemeToggleComponent} from "./theme-toggle/theme-toggle.component";
+import {HeaderComponent} from "./header/header.component";
+import {CondensedHeaderComponent} from "./condensed-header/condensed-header.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [IonApp, IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonList, IonItem, IonLabel, IonListHeader, IonIcon, IonToggle, LetDirective],
+  imports: [IonApp, IonContent, IonHeader, IonToolbar, IonTitle, IonButton, IonList, IonItem, IonLabel, IonListHeader, IonIcon, ThemeToggleComponent, HeaderComponent, CondensedHeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  private readonly darkThemeSubject = new BehaviorSubject(window.matchMedia('(prefers-color-scheme: dark)').matches);
-  readonly isDarkTheme$: Observable<boolean> = merge(
-    this.darkThemeSubject.asObservable(),
-    fromEvent<MediaQueryList>(window.matchMedia('(prefers-color-scheme: dark)'), 'change')
-      .pipe(map((event) => event.matches))
-  ).pipe(tap((isDarkTheme) => document.body.classList.toggle('dark', isDarkTheme)));
+  readonly headerTitle = 'CapacitorCalendar';
 
   constructor() {
     addIcons({
-      'information-circle': informationCircle,
-      'moon': moon
+      'information-circle': informationCircle
     });
-  }
-
-  toggleDarkTheme(isDarkTheme: boolean): void {
-    this.darkThemeSubject.next(!isDarkTheme);
   }
 
   public createEventWithPrompt(): Promise<{ result: CalendarEventActionResult }> {
