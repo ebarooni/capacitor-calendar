417cd22ea95be5b7b7a40f0094b525a7bbd1b49a
ebarooni <barooni.eh@gmail.com>
2024-02-17 22:18:48 +0100

    dispatching plugin logs in logs tab

--- example/src/app/components/permissions-status/permission-status.service.ts
+++ example/src/app/components/permissions-status/permission-status.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
-import {BehaviorSubject, map, merge, Observable, Subject, switchMap} from "rxjs";
+import {BehaviorSubject, filter, map, merge, Observable, Subject, switchMap, tap} from "rxjs";
 import {PermissionState} from "@capacitor/core";
 import {CapacitorCalendar} from "@ebarooni/capacitor-calendar";
+import {LogsService} from "../logs-list/logs.service";
 
 @Injectable({
   providedIn: 'root'
@@ -9,23 +10,34 @@ import {CapacitorCalendar} from "@ebarooni/capacitor-calendar";
 export class PermissionStatusService {
   private readonly checkAllPermissionsSubject = new Subject<void>();
   readonly checkAllPermissions$ = this.checkAllPermissionsSubject.asObservable()
-    .pipe(switchMap(() => CapacitorCalendar.checkAllPermissions()))
+    .pipe(
+      switchMap(() => CapacitorCalendar.checkAllPermissions().catch((error) => error)),
+      tap((response) => this.logsService.dispatchLog(JSON.stringify(response))),
+    )
   private readonly readCalendarPermissionSubject = new BehaviorSubject<void>(void 0);
   readonly readCalendarPermission$: Observable<PermissionState> = merge(
-    this.checkAllPermissions$.pipe(map((result) => result.readCalendar)),
+    this.checkAllPermissions$.pipe(
+      filter((response) => (response?.readCalendar ?? null) !== null),
+      map((result) => result.readCalendar)
+    ),
     this.readCalendarPermissionSubject.asObservable()
       .pipe(
-        switchMap(() => CapacitorCalendar.checkPermission({ alias: 'readCalendar' })),
+        switchMap(() => CapacitorCalendar.checkPermission({alias: 'readCalendar'}).catch((error) => error)),
+        tap((response) => this.logsService.dispatchLog(JSON.stringify(response))),
+        filter((response) => (response?.result ?? null) != null),
         map((response) => response.result)
       )
   );
+
+  constructor(private readonly logsService: LogsService) {}
 
   requestPermission(alias: 'readCalendar'): void {
     switch (alias) {
       case 'readCalendar':
         CapacitorCalendar.requestPermission({ alias: alias })
+          .then((response) => this.logsService.dispatchLog(JSON.stringify(response)))
           .then(() => this.readCalendarPermissionSubject.next())
-          .catch((error) => console.warn(error));
+          .catch((error) => this.logsService.dispatchLog(JSON.stringify(error)));
         break;
       default:
         return;
@@ -34,10 +46,9 @@ export class PermissionStatusService {
 
   requestAllPermissions(): void {
     CapacitorCalendar.requestAllPermissions()
-      .then(() => {
-        this.readCalendarPermissionSubject.next();
-      })
-      .catch((error) => console.warn(error));
+      .then((response) => this.logsService.dispatchLog(JSON.stringify(response)))
+      .then(() => this.readCalendarPermissionSubject.next())
+      .catch((error) => this.logsService.dispatchLog(JSON.stringify(error)));
   }
 
   checkAllPermissions(): void {
