36c862bd0c1430797aec4e4d027772ad0e4c566b
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-02-17 22:43:17 +0100

    showing logs counter on the tab button functional

--- example/src/app/components/logs-list/logs.service.ts
+++ example/src/app/components/logs-list/logs.service.ts
@@ -1,17 +1,28 @@
 import { Injectable } from '@angular/core';
-import {ReplaySubject, scan} from "rxjs";
+import {BehaviorSubject, ReplaySubject, scan, tap} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogsService {
+  private readonly logsNotificationSubject = new BehaviorSubject<number>(0);
+  readonly logsNotification$ = this.logsNotificationSubject.asObservable();
   private readonly logsSubject = new ReplaySubject<string>(1);
   readonly logs$ = this.logsSubject.asObservable()
     .pipe(
       scan((acc: { message: string, timestamp: number }[], curr: string) => [{ message: curr, timestamp: Date.now() }, ...acc], []),
+      tap(() => this.incrementNotificationsCounter())
     );
 
   dispatchLog(log: string): void {
     this.logsSubject.next(log);
+  }
+
+  incrementNotificationsCounter(): void {
+    this.logsNotificationSubject.next(this.logsNotificationSubject.getValue() + 1);
+  }
+
+  resetNotificationsCounter(): void {
+    this.logsNotificationSubject.next(0);
   }
 }
