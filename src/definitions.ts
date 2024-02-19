33617b87e536f8677ba627b3ed14cd6f1ff29dd8
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-02-19 23:05:06 +0100

    added examples for method definitions

--- src/definitions.ts
+++ src/definitions.ts
@@ -2,7 +2,8 @@ import type { PermissionState } from '@capacitor/core';
 
 /**
  * Enum for the possible results of actions performed on a calendar event.
- * This includes creating, editing, and error events.
+ * "saved" for completion of an operation. "Canceled" for a canceled process and "error"
+ * for when something went wrong.
  *
  * @enum
  */
@@ -21,34 +22,37 @@ export interface CalendarPermissionStatus {
   readCalendar: PermissionState;
 }
 
-
-/**
- * Represents the interface for the Calendar plugin in Capacitor.
- *
- * @interface
- */
 export interface CapacitorCalendarPlugin {
   /**
-   * Checks the current permission status of a specific permission.
+   * Checks the current authorization status of a specific permission.
    *
    * @method
+   * @param options An object with the name of the permission
    * @returns {Promise&lt;{ result: PermissionState }&gt;} – A promise that resolves with the current status of the requested permission.
+   * @example
+   * const status = await this.checkPermission({ alias: 'readCalendar' });
    */
   checkPermission(options: { alias: keyof CalendarPermissionStatus }): Promise<{ result: PermissionState }>;
 
   /**
-   * Checks the current permission status for all the required permissions for the plugin.
+   * Checks the current authorization status of all the required permissions for the plugin.
    *
    * @method
-   * @returns {Promise&lt;CalendarPermissionStatus&gt;} – A promise that resolves with the current permissions status.
+   * @returns {Promise&lt;CalendarPermissionStatus&gt;} – A promise that resolves with an object containing all the permissions and their status.
+   * @example
+   * const permissionsStatus = await this.checkAllPermissions();
    */
   checkAllPermissions(): Promise<CalendarPermissionStatus>;
 
   /**
    * Requests authorization to a specific permission, if not already granted.
+   * If the permission is already granted, it will directly return the status.
    *
    * @method
+   * @param options An object with the name of the permission
    * @returns {Promise&lt;{ result: PermissionState }&gt;} – A promise that resolves with the new permission status after the request is made.
+   * @example
+   * const result = await this.requestPermission({ alias: 'readCalendar' });
    */
   requestPermission(options: { alias: keyof CalendarPermissionStatus }): Promise<{ result: PermissionState }>;
 
@@ -57,14 +61,31 @@ export interface CapacitorCalendarPlugin {
    *
    * @method
    * @returns {Promise&lt;CalendarPermissionStatus&gt;} – A promise that resolves with the new permission statuses after the request is made.
+   * @example
+   * const permissionResults = await this.requestAllPermissions();
    */
   requestAllPermissions(): Promise<CalendarPermissionStatus>;
 
   /**
-   * Performs an action (create) on a calendar event by displaying a system prompt to the user.
+   * Creates an event in the calendar by using the native calendar.
+   * On iOS opens a native sheet and on Android opens an intent.
+   * This method does not need any read or write authorization from the user on iOS. However, the entries in the Info.plist file are still needed.
+   * On Android, the user has to authorize for read access.
    *
    * @method
-   * @returns {Promise&lt;{ result: CalendarEventActionResult }&gt;} – A promise that resolves with the result of the event action attempt, which includes the status (saved, canceled, error) and a message in case of an error.
+   * @returns {Promise&lt;{ result: CalendarEventActionResult }&gt;} – A promise that resolves with the result of the action.
+   * @example
+   * let result: CalendarEventActionResult;
+   * if (capacitor.getPlatform() === 'android') {
+   *     const readCalendarStatus = this.requestPermission({ alias: 'readCalendar' }).result;
+   *     if (readCalendarStatus === 'granted') {
+   *         result = await this.createEventWithPrompt();
+   *     } else {
+   *         //  handle the case when user rejects the permission
+   *     }
+   * } else {
+   *     result = await this.createEventWithPrompt();
+   * }
    */
   createEventWithPrompt(): Promise<{ result: CalendarEventActionResult }>;
 }
