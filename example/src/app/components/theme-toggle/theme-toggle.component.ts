03364fe2a43c0f075e2424a032e73f6fcc70b996
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-03-02 19:24:36 +0100

    fixed issue with dark mode using store

--- example/src/app/components/theme-toggle/theme-toggle.component.ts
+++ example/src/app/components/theme-toggle/theme-toggle.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import {IonIcon, IonItem, IonLabel, IonList, IonToggle} from "@ionic/angular/standalone";
 import {LetDirective} from "@ngrx/component";
-import {BehaviorSubject, fromEvent, map, merge, Observable, tap} from "rxjs";
 import {addIcons} from "ionicons";
 import {moon} from "ionicons/icons";
+import {StoreService} from "../../store/store.service";
 
 @Component({
   selector: 'app-theme-toggle',
@@ -19,18 +19,7 @@ import {moon} from "ionicons/icons";
   standalone: true
 })
 export class ThemeToggleComponent {
-  private readonly darkThemeSubject = new BehaviorSubject(window.matchMedia('(prefers-color-scheme: dark)').matches);
-  readonly isDarkTheme$: Observable<boolean> = merge(
-    this.darkThemeSubject.asObservable(),
-    fromEvent<MediaQueryList>(window.matchMedia('(prefers-color-scheme: dark)'), 'change')
-      .pipe(map((event) => event.matches))
-  ).pipe(tap((isDarkTheme) => document.body.classList.toggle('dark', isDarkTheme)));
-
-  constructor() {
+  constructor(readonly storeService: StoreService) {
     addIcons({ 'moon': moon });
-  }
-
-  toggleDarkTheme(isDarkTheme: boolean): void {
-    this.darkThemeSubject.next(!isDarkTheme);
   }
 }
