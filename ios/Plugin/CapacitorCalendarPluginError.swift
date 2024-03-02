392e7ed35253c1fa42c441382cc8da3e9d5f2ef9
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-03-02 18:47:55 +0100

    implemented calendars selection in swift

---
+++ ios/Plugin/CapacitorCalendarPluginError.swift
@@ -0,0 +1,17 @@
+//
+//  PermissionError.swift
+//  Plugin
+//
+//  Created by Ehsan Barooni on 27.02.24.
+//  Copyright © 2024 Max Lynch. All rights reserved.
+//
+
+import Foundation
+
+public enum CapacitorCalendarPluginError: Error {
+    case unknownPermissionStatus
+    case eventStoreAuthorization
+    case viewControllerUnavailable
+    case unknownActionEventCreationPrompt
+    case canceledCalendarsSelectionPrompt
+}
