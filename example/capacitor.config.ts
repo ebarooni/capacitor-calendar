04af240f3bfbdf8e69a6ab3d48771c2ba4245063
ebarooni <barooni.eh@gmail.com>
2024-03-23 16:42:59 +0100

    [capacitor-calendar-25] Replace default linters with self implementation: changed bundle id and display name on ios

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -1,8 +1,8 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'dev.barooni.CapacitorCalendarExample',
-  appName: 'example',
+  appId: 'dev.barooni.CapacitorCalendar',
+  appName: 'CAP Calendar',
   webDir: 'dist/example/browser',
   server: {
     androidScheme: 'https',
