20724a14cc1fe3f33505df80a8582435c19eb8d3
ebarooni <barooni.eh@gmail.com>
2024-03-25 18:45:12 +0100

    [capacitor-calendar-21] Use timestamp in milliseconds instead of date objects for creating reminders and events: updated docs

--- src/schemas/enums/reminder-recurrence-frequency.ts
+++ src/schemas/enums/reminder-recurrence-frequency.ts
@@ -1,6 +1,24 @@
+/**
+ * Enum representing the repeating frequency of the reminder.
+ *
+ * @enum ReminderRecurrenceFrequency
+ * @platform iOS
+ */
 export enum ReminderRecurrenceFrequency {
+  /**
+   * The reminder repeats on a daily basis
+   */
   DAILY,
+  /**
+   * The reminder repeats on a weekly basis
+   */
   WEEKLY,
+  /**
+   * The reminder repeats on a monthly basis
+   */
   MONTHLY,
+  /**
+   * The reminder repeats on a yearly basis
+   */
   YEARLY,
 }
