322075adf8a1537ed51ae48527662f18b7076e60
Ehsan Barooni <code@barooni.dev>
2024-04-01 13:18:39 +0200

    [capacitor-calendar-37] Open reminders app on iOS: updated version

--- example/src/app/store/store.service.ts
+++ example/src/app/store/store.service.ts
@@ -29,7 +29,7 @@ const initialState = <State>{
     [PluginPermission.READ_REMINDERS]: 'prompt',
     [PluginPermission.WRITE_REMINDERS]: 'prompt',
   },
-  appVersion: '0.7.0',
+  appVersion: '0.8.0',
 };
 
 @Injectable()
