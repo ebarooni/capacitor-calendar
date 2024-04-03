730736efddad01697f3d1f294b3f8ba8f2d14e7a
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-04-03 22:12:45 +0200

    [capacitor-calendar-26] Implement finding calendar events: defined type for calendar event

---
+++ src/schemas/interfaces/calendar-event.ts
@@ -0,0 +1,12 @@
+/**
+ * Represents an event in the calendar.
+ *
+ * @interface CalendarEvent
+ * @platform iOS, Android
+ * @property {string} id - The unique identifier of the event.
+ * @property {string} title - The title or name of the event.
+ */
+export interface CalendarEvent {
+  id: string;
+  title: string;
+}
