ad7e45d05e1b4c2f23374dea61754f9b4d3aa80b
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-05-10 00:42:21 -0400

    [capacitor-calendar-80] Delete reminders by id: updated reminder interface

--- src/schemas/interfaces/reminder.ts
+++ src/schemas/interfaces/reminder.ts
@@ -5,6 +5,8 @@
  * @property {string} id The unique identifier of the reminder.
  * @property {string} title The title or name of the reminder. (Optional)
  * @property {string} listId The list that the reminder belongs to. (Optional)
+ * @property {boolean} isCompleted Whether the reminder is completed already or not.
+ * @property {number} priority The priority of the reminder. (Optional)
  */
 export interface Reminder {
   /**
@@ -21,4 +23,14 @@ export interface Reminder {
    * @platform iOS
    */
   listId?: string;
+
+  /**
+   * @platform iOS
+   */
+  isCompleted?: boolean;
+
+  /**
+   * @platform iOS
+   */
+  priority?: number;
 }
