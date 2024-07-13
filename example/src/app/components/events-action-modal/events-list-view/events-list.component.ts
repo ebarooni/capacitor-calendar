2f2b614ec8bf866a9a2c31e5fbc02a357c49a1f8
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-07-13 23:32:08 +0200

    [capacitor-calendar-52] Implement update calendar events: implements events list component

---
+++ example/src/app/components/events-action-modal/events-list-view/events-list.component.ts
@@ -0,0 +1,69 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {
+  IonBackButton,
+  IonButtons,
+  IonCol,
+  IonContent,
+  IonDatetime,
+  IonDatetimeButton,
+  IonGrid,
+  IonHeader,
+  IonIcon,
+  IonItem,
+  IonItemOption,
+  IonItemOptions,
+  IonItemSliding,
+  IonLabel,
+  IonList,
+  IonModal,
+  IonNote,
+  IonRow,
+  IonTitle,
+  IonToolbar,
+} from '@ionic/angular/standalone';
+import { CalendarEvent, CapacitorCalendar } from '@ebarooni/capacitor-calendar';
+import { DatePipe, DOCUMENT } from '@angular/common';
+import { addIcons } from 'ionicons';
+import { create, trash } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-events-list',
+  templateUrl: './events-list.component.html',
+  imports: [
+    IonHeader,
+    IonToolbar,
+    IonContent,
+    IonList,
+    IonItem,
+    IonLabel,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButtons,
+    DatePipe,
+    IonBackButton,
+    IonItemSliding,
+    IonNote,
+    IonDatetimeButton,
+    IonModal,
+    IonDatetime,
+    IonTitle,
+    IonItemOption,
+    IonItemOptions,
+    IonIcon,
+  ],
+  standalone: true,
+})
+export class EventsListComponent {
+  @Input() calendarEvents: CalendarEvent[] = [];
+  @Output() deleteEvent = new EventEmitter<string>();
+  @Output() modifyEvent = new EventEmitter<string>();
+  @Output() done = new EventEmitter<void>();
+
+  constructor() {
+    addIcons({
+      trash: trash,
+      create: create,
+    });
+  }
+}
