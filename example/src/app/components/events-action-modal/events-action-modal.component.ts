973b23f8d0eeb0e0393545b7fd87efd35bcb8b34
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-07-13 23:34:39 +0200

    [capacitor-calendar-52] Implement update calendar events: updates usage of events action modal

---
+++ example/src/app/components/events-action-modal/events-action-modal.component.ts
@@ -0,0 +1,59 @@
+import {
+  Component,
+  EventEmitter,
+  Inject,
+  Output,
+  ViewChild,
+} from '@angular/core';
+import { IonModal } from '@ionic/angular/standalone';
+import { DOCUMENT } from '@angular/common';
+import { BehaviorSubject } from 'rxjs';
+import { CalendarEvent, CapacitorCalendar } from '../../../../../dist/esm';
+import { EventsListComponent } from './events-list-view/events-list.component';
+import { LetDirective } from '@ngrx/component';
+
+@Component({
+  selector: 'app-events-action-modal',
+  templateUrl: './events-action-modal.component.html',
+  styleUrls: ['./events-action-modal.component.scss'],
+  imports: [IonModal, EventsListComponent, LetDirective],
+  standalone: true,
+})
+export class EventsActionModalComponent {
+  @Output() deleteEvents = new EventEmitter<string>();
+  @Output() modifyEvent = new EventEmitter<string>();
+  @ViewChild('modal') modal?: IonModal;
+  public loading = false;
+  public readonly views: 'events-list' = 'events-list';
+  readonly events$ = new BehaviorSubject<CalendarEvent[]>([
+    {
+      title: 'HI',
+      id: '32fqcejmop',
+      calendarId: 'fdbgregh',
+      startDate: Date.now(),
+    } as CalendarEvent,
+  ]);
+
+  constructor(@Inject(DOCUMENT) private readonly document: Document) {}
+
+  present(): Promise<void> {
+    this.loading = true;
+    if (this.modal) {
+      this.modal.presentingElement =
+        this.document.querySelector('app-api.ion-page') ?? undefined;
+      return this.modal.present();
+    } else {
+      this.loading = false;
+      throw new Error('Modal not present');
+    }
+  }
+
+  private fetchEvents(startDate: number, endDate: number): Promise<void> {
+    return CapacitorCalendar.listEventsInRange({
+      startDate: startDate,
+      endDate: endDate,
+    })
+      .then(({ result }) => this.events$.next(result))
+      .catch((error) => console.warn(error));
+  }
+}
