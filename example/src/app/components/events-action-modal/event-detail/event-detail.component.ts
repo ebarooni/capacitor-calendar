efb43a1accfc96cb2b785a28cb2e08c91b9304eb
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2024-07-15 22:57:37 +0200

    [capacitor-calendar-52] Implement update calendar events: implements event detail form

--- example/src/app/components/events-action-modal/event-detail/event-detail.component.ts
+++ example/src/app/components/events-action-modal/event-detail/event-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { addIcons } from 'ionicons';
 import { checkmark } from 'ionicons/icons';
 import {
@@ -7,18 +7,24 @@ import {
   IonButtons,
   IonCol,
   IonContent,
+  IonDatetime,
+  IonDatetimeButton,
   IonGrid,
   IonHeader,
   IonIcon,
   IonInput,
   IonItem,
+  IonLabel,
   IonList,
+  IonModal,
   IonRow,
+  IonTextarea,
   IonTitle,
   IonToggle,
   IonToolbar,
 } from '@ionic/angular/standalone';
 import { CalendarEvent } from '@ebarooni/capacitor-calendar';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-event-detail',
@@ -41,16 +47,67 @@ import { CalendarEvent } from '@ebarooni/capacitor-calendar';
     IonRow,
     IonCol,
     IonToggle,
+    ReactiveFormsModule,
+    IonLabel,
+    IonDatetimeButton,
+    IonModal,
+    IonDatetime,
+    IonTextarea,
   ],
 })
-export class EventDetailComponent {
+export class EventDetailComponent implements OnInit {
   @Input({ required: true }) selectedEvent!: CalendarEvent;
   @Output() closeEventDetailView = new EventEmitter<void>();
-  @Output() saveChangesView = new EventEmitter<void>();
+  @Output() saveChanges = new EventEmitter<typeof this.eventDetailForm.value>();
+  readonly eventDetailForm = new FormGroup({
+    title: new FormControl<string>(''),
+    location: new FormControl<string>(''),
+    isAllDay: new FormControl<boolean>(false),
+    startDate: new FormControl<string>(
+      this.getCurrentIsoTimeInLocalTime(Date.now()),
+    ),
+    endDate: new FormControl<string>(
+      this.getCurrentIsoTimeInLocalTime(Date.now() + 3600 * 1000),
+    ),
+    calendarId: new FormControl<string>(''),
+    url: new FormControl<string>(''),
+    description: new FormControl<string>(''),
+  });
 
   constructor() {
     addIcons({
       checkmark: checkmark,
     });
+  }
+
+  getCurrentIsoTimeInLocalTime(timestamp: number): string {
+    const timezoneOffset = new Date().getTimezoneOffset() * 60000;
+    return new Date(timestamp - timezoneOffset).toISOString().slice(0, -1);
+  }
+
+  ngOnInit(): void {
+    const title = this.selectedEvent?.title ?? '';
+    const location = this.selectedEvent?.location ?? '';
+    const startDate = this.selectedEvent.startDate ?? Date.now();
+    const endDate = this.selectedEvent.endDate ?? Date.now() + 3600 * 1000;
+    const calendarId = this.selectedEvent?.calendarId ?? '';
+    const url = this.selectedEvent?.url ?? '';
+    const description = this.selectedEvent?.description ?? '';
+
+    this.eventDetailForm.setValue({
+      title: title,
+      location: location,
+      isAllDay: this.selectedEvent?.isAllDay ?? false,
+      startDate: this.getCurrentIsoTimeInLocalTime(startDate),
+      endDate: this.getCurrentIsoTimeInLocalTime(endDate),
+      calendarId: calendarId,
+      url: url,
+      description: description,
+    });
+  }
+
+  submitChanges(event: SubmitEvent): void {
+    event.preventDefault();
+    this.saveChanges.emit(this.eventDetailForm.value);
   }
 }
