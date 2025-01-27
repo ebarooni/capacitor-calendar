c99a6a67b7d9be28a9bc03798d837fa62313e26f
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-01-27 21:22:02 +0100

    chore(deps): update to capacitor 7:
    Delete angular example app.

--- example/src/app/pipes/millis-to-iso/millis-to-iso.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MillisToIsoPipe } from './millis-to-iso.pipe';
-
-describe('MillisToIsoPipe', () => {
-  it('create an instance', () => {
-    const pipe = new MillisToIsoPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
