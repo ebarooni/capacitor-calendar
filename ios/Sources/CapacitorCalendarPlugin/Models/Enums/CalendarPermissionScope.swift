24a6a979fec254a4c50c9476e2bf3f84d6a92f09
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-02-04 21:57:49 +0100

    refactor(types): breakdown definitions file (#164)
    Create permission scope enum.

---
+++ ios/Sources/CapacitorCalendarPlugin/Models/Enums/CalendarPermissionScope.swift
@@ -0,0 +1,6 @@
+enum CalendarPermissionScope: String {
+    case readCalendar = "readCalendar"
+    case readReminders = "readReminders"
+    case writeCalendar = "writeCalendar"
+    case writeReminders = "writeReminders"
+}
