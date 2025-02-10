fb9e7f11c073c66724c83835b2f6248aa2dd0eff
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-02-10 22:45:23 +0100

    fix(createEvent): refactor method (#168)
    Add result class.

---
+++ android/src/main/java/dev/barooni/capacitor/calendar/models/results/CreateEventResult.kt
@@ -0,0 +1,19 @@
+package dev.barooni.capacitor.calendar.models.results
+
+import com.getcapacitor.JSObject
+import dev.barooni.capacitor.calendar.PluginError
+import dev.barooni.capacitor.calendar.models.templates.JSResult
+
+data class CreateEventResult(val id: String?): JSResult {
+    init {
+        if (id == null) {
+            throw PluginError.FailedToRetrieveEventId
+        }
+    }
+
+    override fun toJSON(): JSObject {
+        val result = JSObject()
+        result.put("id", id)
+        return result
+    }
+}
