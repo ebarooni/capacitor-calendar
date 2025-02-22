de9250d16b634536d14d3ede7675c0d3cb63cef9
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-02-22 18:28:29 +0100

    perf(calendar): list events (#190)
    Add calendar event enums.

---
+++ src/schemas/enums/event-status.ts
@@ -0,0 +1,26 @@
+/**
+ * @platform Android, iOS
+ * @since 7.1.0
+ */
+export enum EventStatus {
+  /**
+   * @platform iOS
+   * @since 7.1.0
+   */
+  NONE,
+  /**
+   * @platform Android, iOS
+   * @since 7.1.0
+   */
+  CONFIRMED,
+  /**
+   * @platform Android, iOS
+   * @since 7.1.0
+   */
+  TENTATIVE,
+  /**
+   * @platform Android, iOS
+   * @since 7.1.0
+   */
+  CANCELED,
+}
