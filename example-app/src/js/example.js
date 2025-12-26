24b309e0329715ddb23e254e008a33d0b0a40beb
Ehsan Barooni <66495214+ebarooni.noreply@barooni.dev>
2025-12-26 16:23:37 +0100

    feat: capacitor 8
    Upgrade example app to Capacitor 8.

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,6 +1,7 @@
 import { CapacitorCalendar } from '@ebarooni/capacitor-calendar';
 
 window.testEcho = () => {
-  const inputValue = document.getElementById('echoInput').value;
-  CapacitorCalendar.echo({ value: inputValue });
+  CapacitorCalendar.listCalendars().then((calendars) => {
+    console.log(calendars);
+  });
 };
