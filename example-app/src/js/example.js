c5fb0f759950c15a0bf1848c595cad965918f306
Ehsan Barooni <mail@barooni.dev>
2026-06-08 22:47:28 +0200

    feat: add test methods to example app

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,7 +1,18 @@
 import { CapacitorCalendar } from '@ebarooni/capacitor-calendar';
 
-window.testEcho = () => {
-  CapacitorCalendar.listCalendars().then((calendars) => {
-    console.log(calendars);
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelector('#check-all-permissions').addEventListener('click', async () => {
+    const result = await CapacitorCalendar.checkAllPermissions();
+    console.log('#checkAllPermissions', result);
   });
-};
+
+  document.querySelector('#list-calendars').addEventListener('click', async () => {
+    const result = await CapacitorCalendar.listCalendars();
+    console.log('#listCalendars', result);
+  });
+
+  document.querySelector('#request-full-calendar-access').addEventListener('click', async () => {
+    const result = await CapacitorCalendar.requestFullCalendarAccess();
+    console.log('#requestFullCalendarAccess', result);
+  });
+});
