031a9d4e40ae1b1eef6a66a73bc431553647ed55
Ehsan Barooni <mail@barooni.dev>
2026-08-11 22:14:04 +0200

    feat: add delete event and list event in range events to example app

--- example-app/src/js/example.js
+++ example-app/src/js/example.js
@@ -1,4 +1,4 @@
-import { CapacitorCalendar } from '@ebarooni/capacitor-calendar';
+import { CapacitorCalendar, EventSpan } from '@ebarooni/capacitor-calendar';
 
 document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#check-all-permissions').addEventListener('click', async () => {
@@ -16,6 +16,31 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('#createRemindersList', result);
   });
 
+  document.querySelector('#delete-event').addEventListener('click', async () => {
+    await CapacitorCalendar.deleteEvent({
+      id: getEventIdInput().value,
+      span: EventSpan.THIS_AND_FUTURE_EVENTS,
+    });
+  });
+
+  document.querySelector('#delete-event-with-prompt').addEventListener('click', async () => {
+    const result = await CapacitorCalendar.deleteEventWithPrompt({
+      id: getEventIdInput().value,
+      message: 'Are you sure you want to delete this event?',
+      span: EventSpan.THIS_EVENT,
+      title: 'Delete event',
+    });
+    console.log('#deleteEventWithPrompt', result);
+  });
+
+  document.querySelector('#delete-events-by-id').addEventListener('click', async () => {
+    const result = await CapacitorCalendar.deleteEventsById({
+      ids: [getEventIdInput().value],
+      span: EventSpan.THIS_EVENT,
+    });
+    console.log('#deleteEventsById', result);
+  });
+
   document.querySelector('#delete-reminders-list').addEventListener('click', async () => {
     const id = getRemindersListIdInput().value;
     await CapacitorCalendar.deleteRemindersList({ id });
@@ -29,6 +54,25 @@ document.addEventListener('DOMContentLoaded', () => {
   document.querySelector('#list-calendars').addEventListener('click', async () => {
     const result = await CapacitorCalendar.listCalendars();
     console.log('#listCalendars', result);
+  });
+
+  document.querySelector('#list-events-in-range').addEventListener('click', async () => {
+    const now = new Date();
+    const from = new Date();
+    from.setMonth(now.getMonth() - 1);
+    const to = new Date();
+    to.setMonth(now.getMonth() + 1);
+
+    const result = await CapacitorCalendar.listEventsInRange({
+      from: from.getTime(),
+      to: to.getTime(),
+    });
+    console.log('#listEventsInRange', result);
+
+    const event = result.result[0];
+    if (event) {
+      getEventIdInput().value = event.id;
+    }
   });
 
   document.querySelector('#request-full-calendar-access').addEventListener('click', async () => {
@@ -52,6 +96,10 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('#updateRemindersList', result);
   });
 });
+
+function getEventIdInput() {
+  return document.querySelector('#event-id-input');
+}
 
 function getRemindersListIdInput() {
   return document.querySelector('#reminders-list-id-input');
